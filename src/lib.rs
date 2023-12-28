3680892b4406fc3628af758859e1614b61b8601a
y10i <terumi8.neu@icloud.com>
2023-12-28 23:49:32 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,28 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn start_game() {
-    // ゲームの初期化とロジックをここに書きます
-    // 例: カップが落下していくアニメーションなど
+pub struct Block {
+    pub x: i32,
+    pub y: i32,
+    pub visible: bool,
+}
+
+#[wasm_bindgen]
+impl Block {
+    pub fn new() -> Block {
+        Block {
+            x: 0,
+            y: 0,
+            visible: true,
+        }
+    }
+
+    pub fn update_position(&mut self, new_x: i32, new_y: i32) {
+        self.x = new_x;
+        self.y = new_y;
+    }
+
+    pub fn destroy(&mut self) {
+        self.visible = false;
+    }
 }
